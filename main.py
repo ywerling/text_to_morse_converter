5818a3d16166ca829c214698aca64d9f673a35c5
ywerling <ywerling@hotmail.de>
2024-01-10 14:49:42 +0100

    move encoding functionality in a function

--- main.py
+++ main.py
@@ -3,18 +3,20 @@ import morse
 #the user enters a text to be converted to morse
 text_to_encode = input("Enter the text to convert to morse code:")
 
-#prepares the string for conversion and remove non allowed characters
-temp=text_to_encode.lower()
+def morse_encode(text):
+    #prepares the string for conversion, make it all lower case and remove non-allowed characters
+    temp=text.lower()
 
-#encoded_text=morse.STARTING_SIGNAL
-encoded_text=''
+    #encoded_text=morse.STARTING_SIGNAL
+    encoded_text=''
 
-for char in temp:
-    try:
-        encoded_text+=morse.morse_dict[char]
-    except KeyError as ke:
-# if the charcater is unknown a single space will be added
-#        print(ke)
-        encoded_text +=' '
+    for char in temp:
+        try:
+            encoded_text+=morse.morse_dict[char]
+        except KeyError as ke:
+    # if the charcater is unknown a single space will be added
+            encoded_text +=' '
 
-print(f"Message converted to Morse code: {encoded_text}")
+    return encoded_text
+
+print(f"Message converted to Morse code: {morse_encode(text_to_encode)}")
